01464defce3b29afdf81718b2025cb3e81d05c90
FawazMrad <FawazMrad10@gmail.com>
2024-06-04 23:15:43 +0200

    LocalizationAndHelperFiles
    Add some Localization and HelperFiles

---
+++ app/Http/Middleware/LanguageMiddleware.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+use Symfony\Component\HttpFoundation\Response;
+
+class LanguageMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        // Get the language header from the request
+        $lang = $request->header('lang');
+
+        // Set the language based on the header value
+        App::setLocale($lang);
+        return $next($request);
+    }
+}
